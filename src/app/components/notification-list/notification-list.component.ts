da600e441e6a86377f1681c86fec0014632cc753
João Amendola <mx.joaoo@gmail.com>
2022-11-27 20:07:18 -0300

    + variável pessoas ao notification-list (Ex. 7)

--- src/app/components/notification-list/notification-list.component.ts
+++ src/app/components/notification-list/notification-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
   styleUrls: ['./notification-list.component.scss']
 })
-export class NotificationListComponent {
 
+export class NotificationListComponent {
+  pessoas = [
+    {
+      titulo: "Gmail",
+      subtitulo: "Novo e-mail.",
+      data: "20:01 de 22 de Nov. de 2022"
+    },
+    {
+      titulo: "WhatsApp",
+      subtitulo: "Nova mensagem de João",
+      data: "20:43 de 22 de Nov. de 2022"
+    },
+    {
+      titulo: "iFood",
+      subtitulo: "Pedido confirmado!",
+      data: "21:33 de 22 de Nov. de 2022"
+    }
+  ]
 }
