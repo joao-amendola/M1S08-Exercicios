6d4c354c7f18e892fba742cd827f89e9995edf0a
João Amendola <mx.joaoo@gmail.com>
2022-11-27 20:15:44 -0300

    Ex. 8 - valores estáticos@assets/notifications.ts

--- src/app/components/notification-list/notification-list.component.ts
+++ src/app/components/notification-list/notification-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NOTIFICACOES } from 'src/assets/notifications';
 @Component({
   selector: 'ntap-notification-list',
   templateUrl: './notification-list.component.html',
@@ -6,21 +7,6 @@ import { Component } from '@angular/core';
 })
 
 export class NotificationListComponent {
-  pessoas = [
-    {
-      titulo: "Gmail",
-      subtitulo: "Novo e-mail.",
-      data: "20:01 de 22 de Nov. de 2022"
-    },
-    {
-      titulo: "WhatsApp",
-      subtitulo: "Nova mensagem de João",
-      data: "20:43 de 22 de Nov. de 2022"
-    },
-    {
-      titulo: "iFood",
-      subtitulo: "Pedido confirmado!",
-      data: "21:33 de 22 de Nov. de 2022"
-    }
-  ]
+  notifications = NOTIFICACOES
 }
+
---
+++ src/assets/notifications.ts
@@ -0,0 +1,17 @@
+export const NOTIFICACOES = [
+        {
+          titulo: "Gmail",
+          subtitulo: "Novo e-mail.",
+          data: "20:01 de 22 de Nov. de 2022"
+        },
+        {
+          titulo: "WhatsApp",
+          subtitulo: "Nova mensagem de João",
+          data: "20:43 de 22 de Nov. de 2022"
+        },
+        {
+          titulo: "iFood",
+          subtitulo: "Pedido confirmado!",
+          data: "21:33 de 22 de Nov. de 2022"
+        }
+      ]
